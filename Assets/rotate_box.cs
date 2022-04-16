97ba7c156a24cce1e70ab739f49933507e426a91
Jessica so beautiful <jessica.ready.win@gmail.com>
2022-04-16 17:21:20 +0800

    add second scene and third scene

--- Assets/rotate_box.cs
+++ Assets/rotate_box.cs
@@ -10,16 +10,38 @@ public class rotate_box : MonoBehaviour
     private Vector3 rotate_axis;
     void Start()
     {
-        rotate_axis = new Vector3(transform.position.x + 0.25f, transform.position.y - 0.25f, transform.position.z);
-        Debug.Log(rotate_axis);
+        //rotate_axis = new Vector3(transform.position.x + 0.25f, transform.position.y - 0.25f, transform.position.z);
+        //Debug.Log(rotate_axis);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (time > 80)
-        {
-            transform.RotateAround(rotate_axis, Vector3.back, 20 * Time.deltaTime);
-        }
+        //if (time > 80)
+        //{
+          //  transform.RotateAround(rotate_axis, Vector3.back, 20 * Time.deltaTime);
+        //}
+    }
+    public Vector3 TurnRight()
+    {
+        rotate_axis = new Vector3(transform.position.x - 0.25f, transform.position.y - 0.25f, transform.position.z);
+        return rotate_axis;
+    }
+    public Vector3 TurnLeft()
+    {
+        rotate_axis = new Vector3(transform.position.x + 0.25f, transform.position.y - 0.25f, transform.position.z);
+        return rotate_axis;
+    }
+    public Vector3 TurnForward() //backward
+    {
+        //Debug.Log(transform.position);
+        rotate_axis = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z + 0.3f);
+        //Debug.Log(rotate_axis);
+        return rotate_axis;
+    }
+    public Vector3 TurnBack() // forward
+    {
+        rotate_axis = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z - 0.25f);
+        return rotate_axis;
     }
 }
---
+++ Assets/second/tempocontrol.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempocontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int[] gamelist = { 0,1,2,3,0,1,2,3,0,1,2,3};
+    private GameObject[] cubelist = new GameObject[4];
+    public Material basicmat;
+    public Material[] matlist = new Material[4];
+    public int temple = 10;
+    public float time = 0;
+    private bool onlight = false;
+    private GameObject nowcube;
+    private int nowidx = 0; //for gamelist
+    void Start()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            int t = i + 1;
+            string temp = "Cube" + t;
+            //string temp2 = "t" + t;
+            GameObject cube = GameObject.Find(temp);
+            //Material mat = GameObject.Find(temp2).GetComponent;
+            cubelist[i] = cube;
+            //matlist[i] = mat;
+        }
+        //cubelist[gamelist[0]].GetComponent<Renderer>().material = 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < temple && onlight == false)
+        {
+            onlight = true;
+            nowcube = cubelist[gamelist[nowidx]];
+            nowcube.GetComponent<Renderer>().material = matlist[gamelist[nowidx]];
+        }
+        else if(time >= temple)
+        {
+            time =0;
+            nowidx++;
+            nowcube.GetComponent<Renderer>().material = basicmat;
+            onlight = false;
+        }
+        time += Time.deltaTime*3;
+    }
+}
