db30dc4e39e4505b6d8503d6a00db379fe434ede
Jessica so beautiful <jessica.ready.win@gmail.com>
2022-04-17 20:55:21 +0800

    ringcontrol

--- Assets/second/RingControl.cs
+++ Assets/second/RingControl.cs
@@ -13,9 +13,8 @@ public class RingControl : MonoBehaviour
     public class Note
     {
         public float start_time;
-        public float hieght; //radius
         public float end_time;
-        public float spc; //second per cycle
+        public float hieght; // radius
     }
     // Start is called before the first frame update
     private GameObject newArc;
@@ -23,7 +22,7 @@ public class RingControl : MonoBehaviour
     {
     }
 
-    public void CreateRing(Note[] notes)
+    public void CreateRing(Note[] notes,float spc,float hieght_range)
     {
         arcs = new GameObject[notes.Length];
         for (int i = 0; i < notes.Length; i++)
@@ -31,13 +30,14 @@ public class RingControl : MonoBehaviour
             GameObject newarc = Instantiate(arc);
             Material mat_newarc = Instantiate(newarc.GetComponent<Renderer>().material);
 
-            float interval = (notes[i].end_time - notes[i].start_time) / notes[i].spc;
-            float _time = (interval / 2f) + (notes[i].start_time % notes[i].spc) * (_circle_time / notes[i].spc);
+            float interval = (notes[i].end_time - notes[i].start_time) / spc;
+            float _time = (interval / 2f) + (notes[i].start_time) * (_circle_time / spc);
             mat_newarc.SetFloat("StartTime_ ", _time);
             mat_newarc.SetFloat("Alpha_ ", 0);
             // mat_newarc.SetFloat("Radiusinner_", notes[i].hieght);
             // mat_newarc.SetFloat("Radiusouter_ ", notes[i].hieght +2);
             //scale_change
+            mat_newarc.SetFloat("Scale_", 1 + (notes[i].hieght / hieght_range));
             mat_newarc.SetFloat("Angle_ ", interval * _circle_time);
             newarc.GetComponent<Renderer>().material = mat_newarc;
             arcs[i] = newarc;
@@ -46,19 +46,13 @@ public class RingControl : MonoBehaviour
     }
 
     /// <summary>
-    /// input: array
-    ///     ring-arc
-    ///     arc-time
-    ///     starting-point
-    ///     arc-r
-    ///     width
     /// output:
     ///     play()
     /// </summary>
     /// 
     void FirstRing()
     {
-
+        //trigger_enter
 
     }
     // Update is called once per frame
