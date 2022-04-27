1a162b8e17b87fd3a8c3935440cb3b4b68aba25e
voidlotus <alex.eic07g@nctu.edu.tw>
2022-04-28 05:11:14 +0800

    Add scene manager

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -25,24 +26,75 @@ public class SceneTransition : MonoBehaviour
 		"Level_03_MasterRoomScene"
 	};
 
+    // public static SceneNameType currentScene = SceneNameType.Level_00_MainlHallNoPlayerScene;
 	public SceneNameType sceneName;
 
 	private GameObject player;
+    private Mutex switchSceneLock = new Mutex();
+
+    string GetSceneRealName(SceneNameType sceneName)
+    {
+        return levelNames[(int)sceneName];
+    }
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.tag=="PlayerCapsule"){
 			player = GameObject.FindGameObjectWithTag("Player");
-			StartCoroutine(SwitchScene(levelNames[(int)sceneName]));
-		}
-	}
+            //StartCoroutine(SwitchScene(levelNames[(int)sceneName]));
 
-	IEnumerator SwitchScene(string sceneName){
-		Scene currentScene = SceneManager.GetActiveScene();
-		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-		while (!asyncLoad.isDone){yield return null;}
-		SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(sceneName));
-		SceneManager.UnloadSceneAsync(currentScene);
-	}
+            // TODO: ?
+            // SceneNameType nextScene = other.GetComponent<SceneTransition>().sceneName;
 
+            /*
+            SceneNameType nextScene = sceneName;
+            string nextSceneName = getSceneRealName(nextScene);
+
+            if (!SceneManager.GetSceneByName(getSceneRealName(sceneName)).IsValid())
+            {
+                Scene currentScene = SceneManager.GetActiveScene();
+
+                SceneManager.LoadScene(nextSceneName, LoadSceneMode.Additive);
+                SceneManager.SetActiveScene(SceneManager.GetSceneByName(nextSceneName));
+
+                SceneManager.UnloadSceneAsync(currentScene);
+                // Debug.Log(SceneManager.GetActiveScene());
+            }
+            */
+
+            StartCoroutine(SwitchScene(levelNames[(int)sceneName]));
+        }
+    }
+
+    private void Start()
+    {
+        Debug.Assert(SceneManager.GetActiveScene().name == GetSceneRealName(SceneNameType.Level_00_MainlHallScene));
+    }
+
+    // dont destroy : play must loaded-stuff
+    // only switch lv00 lv01 lv02 lv03
+    
+    IEnumerator SwitchScene(string sceneName){
+        switchSceneLock.WaitOne();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name != sceneName)
+        {
+            if (!SceneManager.GetSceneByName(sceneName).IsValid())
+            {
+                AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+                // while (!asyncLoad.isDone) { yield return null; }
+                while (!asyncLoad.isDone) { yield return new WaitForSeconds(0.05f); }
+
+                SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+            }
+
+            // SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(sceneName));
+            SceneManager.UnloadSceneAsync(currentScene);
+        }
+
+        switchSceneLock.ReleaseMutex();
+    }
+    
 }
